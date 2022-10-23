2856d4f4a572b34e6a776e09fdeb6f7993551175
yusgsysh <1394245166@qq.com>
2022-10-23 14:52:33 +0800

    更新16进制

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ typedef  struct
 }SeqStack;
 
 void zhuanhuan (int a,int b){
+    char ch[6]={'A','B','C','D','E','F'};
     SeqStack q;
     q.top=-1;
     do{
@@ -16,12 +17,15 @@ void zhuanhuan (int a,int b){
     }while(a!=0);
 
     for(;q.top>=0;q.top--){
-        printf("%d",q.data[q.top]);
+
+        if(q.data[q.top]>9)printf("%c",ch[q.data[q.top]-10]);
+        else printf("%d",q.data[q.top]);
     }
 }
 
 int main (){
     int a,b;
+    printf("请输入数字和转换为几进制\n");
     scanf("%d %d",&a,&b);
     zhuanhuan(a,b);
 }
